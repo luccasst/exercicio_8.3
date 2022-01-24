b4873180072cb2ee6c46617ea0a4b87d02adfc90
lucas <lucas.c.r.f@hotmail.com>
2022-01-24 19:35:24 -0300

    exercicio

---
+++ exercico1.js
@@ -0,0 +1,11 @@
+const arrays = [
+    ['1', '2', '3'],
+    [true],
+    [4, 5, 6],
+  ];
+  
+  function flatten() {
+    const arrayJuntas = arrays.reduce( (acc, curr) => acc.concat(curr) );
+    return arrayJuntas;
+  }
+  console.log (flatten);
